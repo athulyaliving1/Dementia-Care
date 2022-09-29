3754f385510d6711bc7da7c48ca558e26dbcb8b9
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2022-09-29 10:36:05 +0530

    thank page fix

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <HashRouter>
         <Routes>
           <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
           <Route path="tkpage" element={<Thankpage />} />
           <Route
             path="*"
