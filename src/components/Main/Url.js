2ce8ec0ea8137021192e8f67b46546cd193fe442
athulyaliving1 <110807506+athulyaliving1@users.noreply.github.com>
2022-12-05 14:29:55 +0530

    api fix

--- src/components/Main/Url.js
+++ src/components/Main/Url.js
@@ -1,2 +1,2 @@
 export const UrlLink =
-  "https://contact-app-server-athulya.herokuapp.com/contactdementiacarechennai";
+  "https://contact-formserver-production.up.railway.app/contactdementiacarechennai";
